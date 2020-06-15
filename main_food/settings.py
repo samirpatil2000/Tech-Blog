777fe355b8082f1b708f929d020a0f7008adc61c
samir patil <samirspatil742099@gmail.com>
2020-06-15 18:24:40 +0530

    Small Fixing

--- main_food/settings.py
+++ main_food/settings.py
@@ -131,7 +131,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 CRISPY_TEMPLATE_PACK='bootstrap4'
-LOGIN_REDIRECT_URL='home'
+LOGIN_REDIRECT_URL='index'
 LOGIN_URL='login'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
